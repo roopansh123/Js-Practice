aa0f32780edf3bd0689a113d10b2c27678f9d237
roopansh123 <roopanshsethi29@gmail.com>
2026-05-04 13:40:39 +0530

    commit

---
+++ Java Script/statemgt.js
@@ -0,0 +1,24 @@
+const bankAccount = {
+  owner: "roopansh",
+  balance: 0,
+
+  deposit(amt) {
+    this.balance += amt;
+  },
+
+  withdraw(amt) {
+    if (this.balance < amt) {
+      console.log("insufficient balance");
+    } else {
+      this.balance -= amt;
+    }
+  },
+};
+
+console.log(bankAccount.balance);
+bankAccount.deposit(500);
+console.log(bankAccount.balance);
+bankAccount.withdraw(5000);
+
+bankAccount.withdraw(490);
+console.log(bankAccount.balance);
